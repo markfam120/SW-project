1795def2983dcc20b570576e26b7a695260bf39e
markfam120 <markfam57@gmail.com>
2026-05-11 22:55:47 +0300

    Update database_setup

--- project_SW/src/project/sw/database_setup.sql
+++ project_SW/src/project/sw/database_setup.sql
@@ -6,7 +6,7 @@ GO
 USE CollegeAdmission;
 GO
 
-CREATE TABLE users (
+CREATE TABLE staff (
     Username VARCHAR(50) PRIMARY KEY,
     Password VARCHAR(50) NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
@@ -16,10 +16,26 @@ CREATE TABLE users (
 GO
 
 CREATE TABLE Students (
-    NationalID VARCHAR(20) PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
+    NationalID VARCHAR(20) PRIMARY KEY,
+    Password VARCHAR(50) NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
     Major VARCHAR(50) NOT NULL,
-    GPA FLOAT NOT NULL
 );
 GO
+
+CREATE TABLE admin (
+    Username VARCHAR(50) PRIMARY KEY,
+    Password VARCHAR(50) NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Gender VARCHAR(10) NOT NULL
+    );
+
+
+    insert into dbo.admin values('mark120','12345','mark','michael','male');
+
+    select * from admin;
+
+    
